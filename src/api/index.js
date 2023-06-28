35aa102a3eb973f6da56dddc9401a7f430fcd350
harshdeswal <harshdeswal03@yahoo.com>
2023-06-28 18:56:59 +0530

    Card for each restaurants is done

--- src/api/index.js
+++ src/api/index.js
@@ -13,11 +13,13 @@ export const getPlacesData = async (sw,ne) => {
             tr_latitude:ne.lat,
             bl_longitude: sw.lng,
             tr_longitude: ne.lng,
+            currency: 'INR',
           },
           headers: {
             'X-RapidAPI-Key': '65c5d8d837msh57d48acb96fe2dap19ae23jsndfaa8d48d21c',
             'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-          }});
+          }
+        });
         return data;
 
     }catch(error){
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -31,7 +31,7 @@ const List = ({places}) => {
                 </Select>
             </FormControl>
             <Grid container spacing={3} className={classes.list}>
-                {places && places.map((place,i)=> (
+                { places && places.map((place,i)=> (
                     <Grid item key = {i} xs ={12}>
                         <PlaceDetails place={place}/>
                     </Grid>
