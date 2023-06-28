df9e3e42650ec8badd8cae8e9058a6b0d24dbda9
harshdeswal <harshdeswal03@yahoo.com>
2023-06-29 00:34:33 +0530

    search functionality is left and map configuration left

--- src/App.js
+++ src/App.js
@@ -27,21 +27,23 @@ const App = () => {
     },[rating]);
 
     useEffect(()=> {
-        
-        setIsLoading(true);
-        
-        getPlacesData(type,bounds.sw,bounds.ne)
+        if(bounds.sw && bounds.ne){
+
+            setIsLoading(true);
+            
+            getPlacesData(type,bounds.sw,bounds.ne)
             .then((data)=>{
                 
-                setPlaces(data);
+                setPlaces(data && data.filter((place) => place.name && place.num_reviews > 0));
                 setFilteredPlaces([])
                 setIsLoading(false);
             })
-    },[type,coordinates,bounds]);
+        }
+    },[type,bounds]);
     return (
         <React.Fragment>
             <CssBaseline />
-            <Header />
+            <Header setCoordinates = {setCoordinates} />
             <Grid container spacing={3} style={{width : '100%'}}>
                 <Grid item xs={12} md={4}>
                     <List 
