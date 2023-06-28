c60d816d9fdc4decd32dba719daa57ff3d7e7b1c
harshdeswal <harshdeswal03@yahoo.com>
2023-06-28 17:42:21 +0530

    getting the current location of user done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { CssBaseline,Grid } from '@material-ui/core';
-
+import { getPlacesData } from './api';
+import { useState,useEffect } from 'react';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 const App = () => {
+    const [places,setPlaces] = useState([]);
+    const [coordinates,setCoordinates] = useState({});
+    const [bounds,setBounds] = useState(null);
+    useEffect(()=>{
+        navigator.geolocation.getCurrentPosition(({coords:{latitude,longitude}})=>{
+            setCoordinates({lat:latitude,lng:longitude});
+        })
+    },[]);
+    useEffect(()=> {
+        
+        getPlacesData()
+            .then((data)=>{
+                
+                setPlaces(data);
+            })
+    },[coordinates,bounds]);
     return (
         <React.Fragment>
             <CssBaseline />
@@ -14,7 +31,11 @@ const App = () => {
                     <List />
                 </Grid>
                 <Grid item xs={12} md={8}>
-                    <Map />
+                    <Map 
+                        setCoordinates = {setCoordinates}
+                        setBounds = {setBounds}
+                        coordinates = {coordinates}
+                    />
                 </Grid>
             </Grid>
 
---
+++ src/api/index.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+
+  
+
+export const getPlacesData = async (sw,ne) => {
+    try{
+        const {data:{data}} = await axios.get(URL,{
+          params: {
+            bl_latitude:sw.lat,
+            tr_latitude:ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+          },
+          headers: {
+            'X-RapidAPI-Key': '65c5d8d837msh57d48acb96fe2dap19ae23jsndfaa8d48d21c',
+            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+          }});
+        return data;
+
+    }catch(error){
+        console.log(error);
+    }
+}
